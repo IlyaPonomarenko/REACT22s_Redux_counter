91f8cd6ec08fb1ce23da6da760e81eb40232a8a5
Ilya Ponomarenko <ponomarenkoilya989@gmail.com>
2023-01-27 11:02:28 +0200

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useState } from "react";
+import "./App.css";
+const App = () => {
+  let [counter, updateCounter] = useState(0);
 
-function App() {
+  const addFiveHandler = () => {
+    updateCounter(counter + 5);
+  };
+  const addOneHandler = () => {
+    updateCounter(counter + 1);
+  };
+  const resetHandler = () => {
+    updateCounter((counter = 0));
+  };
+  const removeFiveHandler = () => {
+    updateCounter(counter - 5);
+  };
+  const removeOneHandler = () => {
+    updateCounter(counter - 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="container">
+        <div className="circle">
+          <div>
+            <h1 className="counter">{counter}</h1>
+          </div>
+        </div>
+      </div>
+
+      <div className="btns">
+        <button onClick={addFiveHandler}>Add Five</button>
+        <button onClick={addOneHandler}>Add One</button>
+        <button onClick={resetHandler}>Reset</button>
+        <button onClick={removeOneHandler}>Remove One</button>
+        <button onClick={removeFiveHandler}>Remove Five</button>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
